9e8d1510b8f082fa45d56e20bfdd2f5060eb0635
nqkwey <enagibin@gmail.com>
2017-12-09 17:03:46 +0600

    added RSA decryption and encryption

--- app/src/main/java/com/rosberry/hackathon/Decryptor.java
+++ app/src/main/java/com/rosberry/hackathon/Decryptor.java
@@ -1,11 +1,15 @@
 package com.rosberry.hackathon;
 
+import android.security.keystore.KeyProperties;
 import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
 
 import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
 import java.security.KeyStore;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
 
 import javax.crypto.Cipher;
@@ -18,8 +22,10 @@ import javax.crypto.spec.GCMParameterSpec;
 
 public class Decryptor {
 
+
     private static final String ANDROID_KEY_STORE = "AndroidKeyStore";
     private static final String TRANSFORMATION = "AES/GCM/NoPadding";
+
     private KeyStore keyStore;
     private Storage storage;
 
@@ -33,7 +39,9 @@ public class Decryptor {
         keyStore.load(null);
     }
 
-    String decryptDataWithAES(final String alias) throws Exception {
+    String decryptDataWithAES(String alias) throws Exception {
+        alias = alias.concat("_AES");
+
         String base64InitVector = storage.getString(alias.concat("_initVector"), null);
         String base64Encryption = storage.getString(alias.concat("_encryption"), null);
 
@@ -50,6 +58,28 @@ public class Decryptor {
         cipher.init(Cipher.DECRYPT_MODE, getSecretKey(alias), spec);
 
         return new String(cipher.doFinal(encryptedData), StandardCharsets.UTF_8);
+    }
+
+    String decryptDataWithRSA(String alias) {
+        alias = alias.concat("_RSA");
+        String base64PublicKey = storage.getString(alias.concat("_publicKey"), null);
+        String base64Encryption = storage.getString(alias.concat("_encryption"), null);
+
+        byte[] keyBytes = Base64.decode(base64PublicKey, Base64.NO_WRAP);
+        byte[] encryption = Base64.decode(base64Encryption, Base64.NO_WRAP);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+
+        byte[] decodedBytes = new byte[0];
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance(KeyProperties.KEY_ALGORITHM_RSA);
+            PublicKey key = keyFactory.generatePublic(spec);
+            Cipher cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_RSA);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            decodedBytes = cipher.doFinal(encryption);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new String(decodedBytes, StandardCharsets.UTF_8);
     }
 
     private SecretKey getSecretKey(final String alias) throws Exception {
--- app/src/main/java/com/rosberry/hackathon/Encryptor.java
+++ app/src/main/java/com/rosberry/hackathon/Encryptor.java
@@ -4,9 +4,16 @@ import android.os.Build;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
 import android.util.Base64;
+import android.util.Log;
 
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
@@ -19,9 +26,9 @@ import javax.crypto.SecretKey;
 public class Encryptor {
 
     private static final String ANDROID_KEY_STORE = "AndroidKeyStore";
+
     private static final String TRANSFORMATION = "AES/GCM/NoPadding";
-    private byte[] initVector;
-    private byte[] encryption;
+
     private Storage storage;
 
     Encryptor(Storage storage) {
@@ -29,16 +36,27 @@ public class Encryptor {
     }
 
     byte[] encryptTextWithAES(String alias, String textToEncrypt) throws Exception {
+
+        alias = alias.concat("_AES");
+
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
         cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(alias, KeyProperties.KEY_ALGORITHM_AES));
 
-        initVector = cipher.getIV();
-        encryption = cipher.doFinal(textToEncrypt.getBytes(StandardCharsets.UTF_8));
+        byte[] initVector = cipher.getIV();
+        byte[] encryption = cipher.doFinal(textToEncrypt.getBytes(StandardCharsets.UTF_8));
         String base64InitVector = Base64.encodeToString(initVector, Base64.NO_WRAP);
         String base64Encryption = Base64.encodeToString(encryption, Base64.NO_WRAP);
         storage.putString(alias.concat("_initVector"), base64InitVector);
         storage.putString(alias.concat("_encryption"), base64Encryption);
+
+        Log.d("AndroidKeyStore", "aliases: " + getAllAliasesInTheKeystore());
         return encryption;
+    }
+
+    private ArrayList<String> getAllAliasesInTheKeystore() throws Exception {
+        KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+        keyStore.load(null);
+        return Collections.list(keyStore.aliases());
     }
 
     private SecretKey getSecretKey(String alias, String algorithm) throws Exception {
@@ -58,11 +76,37 @@ public class Encryptor {
         return keyGenerator.generateKey();
     }
 
-    byte[] getInitVector() {
-        return initVector;
-    }
+    byte[] encryptTextWithRSA(String alias, String textToEncrypt) {
+        alias = alias.concat("_RSA");
+        Key publicKey = null;
+        Key privateKey = null;
+        try {
+            getAllAliasesInTheKeystore();
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA);
+            kpg.initialize(1024);
+            KeyPair kp = kpg.genKeyPair();
+            publicKey = kp.getPublic();
+            privateKey = kp.getPrivate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-    byte[] getEncryption() {
-        return encryption;
+        // Encode the original data with RSA private key
+        byte[] encodedBytes = null;
+        try {
+            Cipher c = Cipher.getInstance("RSA");
+            c.init(Cipher.ENCRYPT_MODE, privateKey);
+            encodedBytes = c.doFinal(textToEncrypt.getBytes());
+            String base64PublicKey = Base64.encodeToString(publicKey.getEncoded(), Base64.NO_WRAP);
+            String base64Encrypted = Base64.encodeToString(encodedBytes, Base64.NO_WRAP);
+
+            storage.putString(alias.concat("_publicKey"), base64PublicKey);
+            storage.putString(alias.concat("_encryption"), base64Encrypted);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return encodedBytes;
     }
 }
